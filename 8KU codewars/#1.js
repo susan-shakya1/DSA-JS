1d77f172bfcbbebc41584f9a9e26b11644992d18
susan-shakya1 <susanshakya2059@gmail.com>
2024-02-29 09:41:09 +0545

    solving DSA Question

--- 8KU codewars/#1.js
+++ 8KU codewars/#1.js
@@ -1,4 +1,4 @@
-// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry about strings with less than two characters.
+// !It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry about strings with less than two characters.
 
 function removeChar(str){
 // slice() extracts a part of a string and returns the extracted part:
---
+++ 8KU codewars/#4.js
@@ -0,0 +1,30 @@
+// !Given an array of integers your solution should find the smallest integer.
+
+// For example:
+
+// Given [34, 15, 88, 2] your solution will return 2
+// Given [34, -345, -1, 100] your solution will return -345
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      return Math.min(...args)
+    }
+  }
+
+//   OR
+
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      var lowest;
+      for(var i in args){
+        if(i==0){
+          lowest = args[i];
+        }
+        else {
+          if(lowest >= args[i]){
+          lowest = args[i];
+          }
+        }
+      }
+      return lowest;
+    }
+  }
