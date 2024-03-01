010db4890c608e7462cedc9f78553b447f101554
susan-shakya1 <susanshakya2059@gmail.com>
2024-03-01 21:30:44 +0545

    DSA

--- 8KU codewars/#5.js
+++ 8KU codewars/#5.js
@@ -30,3 +30,13 @@ function century(year) {
 //   OR
 
 const century = year => year % 100 === 0 ? parseInt(year / 100) : parseInt(year / 100) + 1;
+
+
+
+// !Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+// !You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+function litres(time) {
+  return Math.floor(time * 0.5);
+}
